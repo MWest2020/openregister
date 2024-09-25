276f6c7e0d20a3948fd148dd56d434cda405ea2c
Thijn <Thijn@conduction.nl>
2024-09-25 16:36:49 +0200

    finished source modal

--- src/modals/Modals.vue
+++ src/modals/Modals.vue
@@ -3,18 +3,24 @@
 	<div>
 		<EditRegister />
 		<DeleteRegister />
+		<EditSource />
+		<DeleteSource />
 	</div>
 </template>
 
 <script>
 import EditRegister from './register/EditRegister.vue'
 import DeleteRegister from './register/DeleteRegister.vue'
+import EditSource from './source/EditSource.vue'
+import DeleteSource from './source/DeleteSource.vue'
 
 export default {
 	name: 'Modals',
 	components: {
 		EditRegister,
 		DeleteRegister,
+		EditSource,
+		DeleteSource,
 	},
 }
 </script>
