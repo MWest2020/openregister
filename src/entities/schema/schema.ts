42d59904754e5b1c7a82ca993d6c5aa61173dd59
Thijn <Thijn@conduction.nl>
2024-09-25 16:05:06 +0200

    lint fix

--- src/entities/schema/schema.ts
+++ src/entities/schema/schema.ts
@@ -2,29 +2,31 @@ import { SafeParseReturnType, z } from 'zod'
 import { TSchema } from './schema.types'
 
 export class Schema implements TSchema {
-    public id: string
-    public name: string
-    public description: string
-    public jsonSchema: object
-    public version: string
 
-    constructor(schema: TSchema) {
-        this.id = schema.id || ''
-        this.name = schema.name || ''
-        this.description = schema.description || ''
-        this.jsonSchema = schema.jsonSchema || {}
-        this.version = schema.version || '1.0.0'
-    }
+	public id: string
+	public name: string
+	public description: string
+	public jsonSchema: object
+	public version: string
 
-    public validate(): SafeParseReturnType<TSchema, unknown> {
-        const schema = z.object({
-            id: z.string().min(1),
-            name: z.string().min(1),
-            description: z.string(),
-            jsonSchema: z.object({}),
-            version: z.string(),
-        })
+	constructor(schema: TSchema) {
+		this.id = schema.id || ''
+		this.name = schema.name || ''
+		this.description = schema.description || ''
+		this.jsonSchema = schema.jsonSchema || {}
+		this.version = schema.version || '1.0.0'
+	}
 
-        return schema.safeParse(this)
-    }
+	public validate(): SafeParseReturnType<TSchema, unknown> {
+		const schema = z.object({
+			id: z.string().min(1),
+			name: z.string().min(1),
+			description: z.string(),
+			jsonSchema: z.object({}),
+			version: z.string(),
+		})
+
+		return schema.safeParse(this)
+	}
+
 }
--- src/entities/source/source.ts
+++ src/entities/source/source.ts
@@ -2,26 +2,28 @@ import { SafeParseReturnType, z } from 'zod'
 import { TSource } from './source.types'
 
 export class Source implements TSource {
-    public id: string
-    public name: string
-    public description: string
-    public databaseUrl: string
 
-    constructor(source: TSource) {
-        this.id = source.id || ''
-        this.name = source.name || ''
-        this.description = source.description || ''
-        this.databaseUrl = source.databaseUrl || ''
-    }
+	public id: string
+	public name: string
+	public description: string
+	public databaseUrl: string
 
-    public validate(): SafeParseReturnType<TSource, unknown> {
-        const schema = z.object({
-            id: z.string().min(1),
-            name: z.string().min(1),
-            description: z.string(),
-            databaseUrl: z.string().url(),
-        })
+	constructor(source: TSource) {
+		this.id = source.id || ''
+		this.name = source.name || ''
+		this.description = source.description || ''
+		this.databaseUrl = source.databaseUrl || ''
+	}
 
-        return schema.safeParse(this)
-    }
+	public validate(): SafeParseReturnType<TSource, unknown> {
+		const schema = z.object({
+			id: z.string().min(1),
+			name: z.string().min(1),
+			description: z.string(),
+			databaseUrl: z.string().url(),
+		})
+
+		return schema.safeParse(this)
+	}
+
 }
