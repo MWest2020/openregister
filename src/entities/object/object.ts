c11ec59fbf67092522822586ec1077784c80e656
Thijn <thijn@conduction.nl>
2024-09-30 14:30:09 +0200

    added object pages, store and entity

---
+++ src/entities/object/object.ts
@@ -0,0 +1,38 @@
+import { SafeParseReturnType, z } from 'zod'
+import { TObject } from './object.types'
+
+export class Object implements TObject {
+
+	public id: string
+	public uuid: string
+	public register: string
+	public schema: string
+	public object: string
+	public updated: string
+	public created: string
+
+	constructor(object: TObject) {
+		this.id = object.id || ''
+		this.uuid = object.uuid
+		this.register = object.register
+		this.schema = object.schema
+		this.object = object.object
+		this.updated = object.updated || ''
+		this.created = object.created || ''
+	}
+
+	public validate(): SafeParseReturnType<TObject, unknown> {
+		const schema = z.object({
+			id: z.string().min(1),
+			uuid: z.string().min(1),
+			register: z.string().min(1),
+			schema: z.string().min(1),
+			object: z.string(),
+			updated: z.string(),
+			created: z.string(),
+		})
+
+		return schema.safeParse(this)
+	}
+
+}
