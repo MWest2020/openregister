bb7cbe850b84ed98bd8b17835cedad8950ce9efe
Ruben van der Linde <rubenvdlinde@gmail.com>
2025-02-08 08:21:11 +0100

    Lets retry the config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,26 +1,9 @@
-
-const config = {
-  title: 'Open Register',
-  tagline: 'Flexible object management for Nextcloud',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://conductionnl.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/openregister/',
-
-  // GitHub pages deployment config
-  organizationName: 'conductionnl',
-  projectName: 'openregister',
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-};
+export default async function createConfigAsync() {
+    return {
+      title: 'Open Register',
+      tagline: 'Flexible object management for Nextcloud',
+      url: 'https://conductionnl.github.io',
+      baseUrl: '/openregister/'
+      // your site config ...
+    };
+  }
