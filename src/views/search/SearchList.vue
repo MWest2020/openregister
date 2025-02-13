264bd9f5a75de399085788ed3595b4554a5c23e3
Thijn <thijn@conduction.nl>
2025-02-13 15:23:40 +0100

    lint-fix

--- src/views/search/SearchList.vue
+++ src/views/search/SearchList.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { searchStore, navigationStore } from '../../store/store.js'
-import _ from 'lodash'
+import { searchStore } from '../../store/store.js'
 </script>
 
 <template>
@@ -11,24 +10,10 @@ import _ from 'lodash'
 	</ul>
 </template>
 <script>
-import { NcListItem, NcActionButton } from '@nextcloud/vue'
-
-// Icons
-import ListBoxOutline from 'vue-material-design-icons/ListBoxOutline.vue'
-import ArchiveOutline from 'vue-material-design-icons/ArchiveOutline.vue'
-import Pencil from 'vue-material-design-icons/Pencil.vue'
-import AlertOutline from 'vue-material-design-icons/AlertOutline.vue'
-import OpenInApp from 'vue-material-design-icons/OpenInApp.vue'
-import OpenInNew from 'vue-material-design-icons/OpenInNew.vue'
 
 export default {
 	name: 'SearchList',
 	components: {
-		NcListItem,
-		NcActionButton,
-		// Icons
-		ListBoxOutline,
-		OpenInNew,
 	},
 	mounted() {
 		// something
