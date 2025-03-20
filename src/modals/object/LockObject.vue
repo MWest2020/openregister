713f448c599caa69f0ef88fb781cc202183bae3a
Thijn <thijn@conduction.nl>
2025-03-20 13:08:44 +0100

    Update LockObject.vue
    I made a mistake in my previous suggestion

--- src/modals/object/LockObject.vue
+++ src/modals/object/LockObject.vue
@@ -10,7 +10,7 @@ import { objectStore, navigationStore } from '../../store/store.js'
 		<p v-if="success === null" class="lockP">
 			Do you want to lock <b>{{ objectStore.objectItem?.uuid }}</b>?
 		</p>
-		<p class="lockP">
+		<p v-if="success === null" class="lockP">
 			Locking an object prevents other users from modifying it until it is unlocked.
 			You can specify an optional process name to indicate why it's locked and a duration after which it will automatically unlock.
 			Only the user who locked the object or an administrator can unlock it before the duration expires.
