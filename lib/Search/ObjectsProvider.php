06e8107e43e3f93e93c9e5606bc84d766ad7c5ba
Ruben van der Linde <rubenvdlinde@gmail.com>
2025-03-21 08:47:27 +0100

    Ad search profider for REGISTERS-125

---
+++ lib/Search/ObjectsProvider.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\OpenRegister\Search;
+
+use OCP\IUser;
+use OCP\Search\FilterDefinition;
+use OCP\Search\IFilteringProvider;
+use OCP\Search\ISearchQuery;
+use OCP\Search\SearchResult;
+use OCP\Search\SearchResultEntry;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+
+use OCA\OpenRegister\Service\ObjectService;
+use OCA\OpenRegister\Service\SearchService; //Todo do we need this
+
+class ObjectsProvider implements IFilteringProvider {
+
+    private IL10N $l10n;
+    private IURLGenerator $urlGenerator;
+
+    public function __construct(IL10N $l10n, IURLGenerator $urlGenerator) {
+        $this->l10n = $l10n;
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function getSupportedFilters(): array {
+        return [
+            // Generic
+            'term',
+            'since',
+            'until',
+            'person',
+            //Open Register Specific
+            'register',
+            'schema',
+        ];
+    }
+
+    public function getAlternateIds(): array {
+        return [];
+    }
+
+    public function getCustomFilters(): array {
+        return [
+            new FilterDefinition('register', FilterDefinition::TYPE_STRING),
+            new FilterDefinition('schema', FilterDefinition::TYPE_STRING),
+        ];
+    }
+
+    public function search(IUser $user, ISearchQuery $query): SearchResult {
+        // Retrieve filters
+        $filters = [];
+        /** @var $register ?string */
+        $register = $query->getFilter('register')?->get();
+        if ($register !== null) {
+            $filters['register'] = $register;
+        }
+
+        $schema = $query->getFilter('schema')?->get();
+        if ($schema !== null) {
+            $filters['schema'] = $schema;
+        }
+
+        /** @var $term ?string */
+        $search = $query->getFilter('term')?->get();
+        /** @var $since ?string */
+        $since = $query->getFilter('since')?->get();
+        /** @var $until ?string */
+        $until = $query->getFilter('until')?->get();
+
+        //@todo: implement pagination
+        $limit = null;
+        $offset = null;
+        $order = null;
+
+        // Get the objects
+        $results = $this->objectEntityMapper->findAll(
+            limit: $limit,
+            offset: $offset,
+            filters: $filters,
+            sort: $order,
+            search: $search
+        );
+
+        // Convert results to SearchResult
+        $searchResultEntries = [];
+        foreach ($results as $result) {
+            $searchResultEntries[] = new SearchResultEntry(
+                $this->urlGenerator->linkToRoute(
+                    'openregister.objects.show',
+                    ['id' => $result->getUuid()]
+                ),
+                $result->getUuid(),
+                'An Open Register Object', // @todo: add regsiter and schema to the description
+                $this->urlGenerator->linkToRoute(
+                    'openregister.objects.show',
+                    ['id' => $result->getUuid()]
+                )
+            );
+        }
+
+        return SearchResult::complete(
+            $this->l10n->t('Open Register'),
+            $searchResultEntries
+        );
+    }
+}
