068e90ebac0c230f3b885931bd41b6b4def18fc6
Alex Rekachynskyi <phone.070718@gmail.com>
2025-05-13 13:59:01 +0200

    Table view on schemas and registers
    (website may blow up)

--- src/store/modules/schema.js
+++ src/store/modules/schema.js
@@ -7,13 +7,21 @@ export const useSchemaStore = defineStore('schema', {
 		schemaItem: false,
 		schemaPropertyKey: null, // holds a UUID of the property to edit
 		schemaList: [],
+		viewMode: 'cards',
 		filters: [], // List of query
 		pagination: {
 			page: 1,
 			limit: 20,
 		},
 	}),
+	getters: {
+		getViewMode: (state) => state.viewMode,
+	},
 	actions: {
+		setViewMode(mode) {
+			this.viewMode = mode
+			console.log('View mode set to:', mode)
+		},
 		setSchemaItem(schemaItem) {
 			this.schemaItem = schemaItem && new Schema(schemaItem)
 			console.log('Active schema item set to ' + (schemaItem?.title || 'null'))
@@ -42,6 +50,10 @@ export const useSchemaStore = defineStore('schema', {
 		setFilters(filters) {
 			this.filters = { ...this.filters, ...filters }
 			console.info('Query filters set to', this.filters) // Logging the filters
+		},
+		setViewMode(mode) {
+			this.viewMode = mode
+			console.log('View mode set to:', mode)
 		},
 		/* istanbul ignore next */ // ignore this for Jest until moved into a service
 		async refreshSchemaList(search = null) {
--- src/views/schema/SchemasIndex.vue
+++ src/views/schema/SchemasIndex.vue
@@ -9,6 +9,27 @@ import formatBytes from '../../services/formatBytes.js'
 			<h2 class="pageHeader">
 				Schemas
 			</h2>
+
+			<div class="viewModeSwitchContainer">
+					<button 
+						class="switch-option" 
+						:class="{ active: schemaStore.viewMode === 'table' }"
+						@click="schemaStore.setViewMode('table')"
+					>
+						<span>{{ t('openregister', 'Tabel') }}</span>
+						<TableOfContentsIcon :size="20" />
+					</button>
+					<div class="switch-slider" :style="{ left: schemaStore.viewMode === 'table' ? '0' : '50%' }"></div>
+					<button 
+						class="switch-option" 
+						:class="{ active: schemaStore.viewMode === 'cards' }"
+						@click="schemaStore.setViewMode('cards')"
+					>
+						<span>{{ t('openregister', 'Kaart') }}</span>
+						<CardOutlineIcon :size="20" />
+					</button>
+				</div>
+
 			<NcActions
 				:force-name="true"
 				:inline="1"
@@ -34,7 +55,8 @@ import formatBytes from '../../services/formatBytes.js'
 				<NcEmptyContent title="No schemas found" icon="icon-folder" />
 			</div>
 			<div v-else class="registers">
-				<div v-for="schema in schemaStore.schemaList" :key="schema.id" class="registerCard">
+				<template v-if="schemaStore.viewMode === 'cards'"> 
+					<div v-for="schema in schemaStore.schemaList" :key="schema.id" class="registerCard">
 					<div class="registerHeader">
 						<h2>
 							<FileTreeOutline :size="20" />
@@ -147,44 +169,6 @@ import formatBytes from '../../services/formatBytes.js'
 						</table>
 					</div>
 					<div v-else>
-						<table class="statisticsTable schemaStats">
-							<thead>
-								<tr>
-									<th>{{ t('openregister', 'Name') }}</th>
-									<th>{{ t('openregister', 'Type') }}</th>
-									<th>{{ t('openregister', 'Actions') }}</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr v-for="(property, key) in schema.properties" :key="key">
-									<td>{{ key }}</td>
-									<td>{{ property.type }}</td>
-									<td>
-										<NcActions :primary="false">
-											<NcActionButton :aria-label="'Edit ' + key"
-												@click="schemaStore.setSchemaPropertyKey(key); schemaStore.setSchemaItem(schema); navigationStore.setModal('editSchemaProperty')">
-												<template #icon>
-													<Pencil :size="16" />
-												</template>
-												Edit
-											</NcActionButton>
-											<NcActionButton :aria-label="'Delete ' + key"
-												@click="schemaStore.setSchemaPropertyKey(key); schemaStore.setSchemaItem(schema); navigationStore.setModal('deleteSchemaProperty')">
-												<template #icon>
-													<TrashCanOutline :size="16" />
-												</template>
-												Delete
-											</NcActionButton>
-										</NcActions>
-									</td>
-								</tr>
-								<tr v-if="!Object.keys(schema.properties).length">
-									<td colspan="3">
-										No properties found
-									</td>
-								</tr>
-							</tbody>
-						</table>
 					</div>
 					<!-- Toggle button -->
 					<NcButton @click="schema.showProperties = !schema.showProperties">
@@ -195,6 +179,74 @@ import formatBytes from '../../services/formatBytes.js'
 						{{ schema.showProperties ? 'Show Stats' : 'Show Properties' }}
 					</NcButton>
 				</div>
+			</template>
+				<template v-else>
+					<table class="statisticsTable registerStats tableOfContents">
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Objects (Total/Size)</th>
+								<th>Logs (Total/Size)</th>
+								<th>Files (Total/Size)</th>
+								<th>Registers</th>
+								<th>Created</th>
+								<th>Updated</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="schema in schemaStore.schemaList" :key="schema.id">
+								<td>{{ schema.title }}</td>
+								<td>{{ schema.stats?.objects?.total || 0 }}/{{ formatBytes(schema.stats?.objects?.size || 0) }}</td>
+								<td>{{ schema.stats?.logs?.total || 0 }}/{{ formatBytes(schema.stats?.logs?.size || 0) }}</td>
+								<td>{{ schema.stats?.files?.total || 0 }}/{{ formatBytes(schema.stats?.files?.size || 0) }}</td>
+								<td>{{ schema.stats?.registers|| 0 }}</td>
+								<td>{{ schema.created ? new Date(schema.created).toLocaleDateString({day: '2-digit', month: '2-digit', year: 'numeric'}) + ', ' + new Date(schema.created).toLocaleTimeString({hour: '2-digit', minute: '2-digit', second: '2-digit'}) : '-' }}</td>
+								<td>{{ schema.updated ? new Date(schema.updated).toLocaleDateString({day: '2-digit', month: '2-digit', year: 'numeric'}) + ', ' + new Date(schema.updated).toLocaleTimeString({hour: '2-digit', minute: '2-digit', second: '2-digit'}) : '-' }}</td>
+								<td>
+									<NcActions :primary="false">
+										<template #icon>
+											<DotsHorizontal :size="20" />
+										</template>
+										<NcActionButton @click="schemaStore.setSchemaItem(schema); navigationStore.setModal('editSchema')">
+											<template #icon>
+												<Pencil :size="20" />
+											</template>
+											Edit
+										</NcActionButton>
+										<NcActionButton @click="schemaStore.setSchemaPropertyKey(null); schemaStore.setSchemaItem(schema); navigationStore.setModal('editSchemaProperty')">
+											<template #icon>
+												<PlusCircleOutline :size="20" />
+											</template>
+											Add Property
+										</NcActionButton>
+										<NcActionButton @click="schemaStore.downloadSchema(schema)">
+											<template #icon>
+												<Download :size="20" />
+											</template>
+											Download
+										</NcActionButton>
+										<NcActionButton
+											v-tooltip="schema.stats?.objects?.total > 0 ? 'Cannot delete: objects are still attached' : ''"
+											:disabled="schema.stats?.objects?.total > 0"
+											@click="schemaStore.setSchemaItem(schema); navigationStore.setDialog('deleteSchema')">
+											<template #icon>
+												<TrashCanOutline :size="20" />
+											</template>
+											Delete
+										</NcActionButton>
+										<NcActionButton @click="schemaStore.setSchemaItem(schema); navigationStore.setSelected('schemaDetails')">
+											<template #icon>
+												<InformationOutline :size="20" />
+											</template>
+											View Details
+										</NcActionButton>
+									</NcActions>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</template>
 			</div>
 		</div>
 	</NcAppContent>
@@ -212,6 +264,8 @@ import Refresh from 'vue-material-design-icons/Refresh.vue'
 import InformationOutline from 'vue-material-design-icons/InformationOutline.vue'
 import TableIcon from 'vue-material-design-icons/Table.vue'
 import ListIcon from 'vue-material-design-icons/FormatListBulleted.vue'
+import TableOfContentsIcon from 'vue-material-design-icons/TableOfContents.vue'	
+import CardOutlineIcon from 'vue-material-design-icons/CardOutline.vue'
 
 export default {
 	name: 'SchemasIndex',
@@ -309,6 +363,39 @@ export default {
 	padding: 8px;
 }
 
+.statisticsTable {
+	width: 100%;
+	border-collapse: collapse;
+	font-size: 0.9em;
+	background: var(--color-main-background);
+	border-radius: var(--border-radius);
+	overflow: hidden;
+
+	th, td {
+		padding: 8px 12px;
+		text-align: start;
+		border-block-end: 1px solid var(--color-border);
+	}
+
+	th {
+		background-color: var(--color-background-darker);
+		color: var(--color-text-maxcontrast);
+		font-weight: normal;
+	}
+
+	tr:last-child td {
+		border-block-end: none;
+	}
+
+	.subRow td {
+		color: var(--color-text-maxcontrast);
+	}
+
+	.indented {
+		padding-inline-start: 24px;
+	}
+}
+
 .statisticsTable.schemaStats {
 	width: 100%;
 	border-collapse: collapse;
@@ -334,5 +421,57 @@ export default {
 .statisticsTable.schemaStats tr:last-child td {
 	border-block-end: none;
 }
+.viewModeSwitchContainer {
+	display: flex;
+	align-items: center;
+	border: 1px solid var(--color-border);
+	border-radius: 8px;
+	overflow: hidden;
+	position: relative;
+	height: 34px;
+	margin-left: auto;
+	margin-right: 15px;
+	.switch-option {
+		display: flex;
+		width: 80px;
+		align-items: center;
+		justify-content: center;
+		gap: 8px;
+		background: transparent;
+		position: relative;
+		z-index: 1;
+		color: var(--color-primary-element-light-text);
 
+		&.active {
+			font-weight: 700;
+		}
+	}
+	.switch-slider {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 50%;
+		height: 100%;
+		background-color: var(--color-primary-element-light);
+		transition: left .2s ease-in-out;
+		pointer-events: none;
+		z-index: 0;
+	}
+}
+.tableOfContents {
+	thead {
+		th {
+			background-color: var(--color-primary-light);
+			font-weight: 600;
+		}
+	}
+	tbody {
+		tr:nth-child(odd) {
+			background-color: transparent;
+		}
+		tr:nth-child(even) {
+			background-color: var(--color-border);
+		}
+	}
+}
 </style>
